2bdedec1dc9e1e58a6c457d2f458048cc946fb64
three18ti <three18ti@gmail.com>
2014-05-12 09:31:20 -0600

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'hvm-base'
+maintainer       'three18ti'
+maintainer_email 'three18ti@gmail.com'
+license          'MIT'
+description      'Installs/Configures hvm-base'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
